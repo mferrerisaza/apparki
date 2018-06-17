7a3f1c52158c7add61dda24ffbf09335d89b5e07
Miguel Ferrer <miguef7@gmail.com>
2018-06-16 22:39:08 -0500

    add regex for plate and translate forms and erros

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,7 @@ class Vehicle < ApplicationRecord
 
   validates :plate, presence: true
   validates :plate, uniqueness: true
+
+  col_plate_regex = /([A-Z]{3}\d{3})|([A-Z]{2}\d{4})|([A-Z]{1}\d{4})|([A-Z]{3}\d{2}[A-Z])/
+  validates :plate, format: { with: col_plate_regex }
 end
