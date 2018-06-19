9badf6b6a544d65c25dce3c9ca169f968ee79ba6
Miguel Ferrer <miguef7@gmail.com>
2018-06-19 01:38:13 -0500

    add method to update charge of a ticket instance

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,7 @@ class Ticket < ApplicationRecord
   belongs_to :parking_zone
   belongs_to :vehicle
 
+  monetize :charge_cents
   mount_uploader :picture, PhotoUploader
 
   validates :entry, :status, presence: true
@@ -10,5 +11,12 @@ class Ticket < ApplicationRecord
 
   def self.search_by_plate(query)
     joins(:vehicle).where("plate ILIKE ?", "%#{query}%")
+  end
+
+  def update_charge
+    time_parked = (exit-entry)/1.hour
+    grace_time = time_parked.floor + 0.25
+    hours = grace_time >= time_parked ? time_parked.floor : time_parked.ceil
+    self.charge = hours * parking_zone.price
   end
 end
