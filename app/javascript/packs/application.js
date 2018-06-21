312c8af907fa237e93a11fe7faa3d9bfa40b1272
Miguel Ferrer <miguef7@gmail.com>
2018-06-21 09:02:15 -0500

    add menu component and import first version to index

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,10 @@ import "../components/textField.js";
 import "../components/select.js";
 import "../components/imagePreview.js";
 import "../components/navbar.js";
+import {MDCMenu} from '@material/menu';
+
+const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+const menuButtonEl = document.querySelector('#menu-button');
+menuButtonEl.addEventListener('click', function() {
+    menu.open = !menu.open;
+});
