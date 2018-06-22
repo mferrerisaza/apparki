60bf4eca38f30d7915ae8107020bf71bde17abee
Daniel Lopez <dlopez.10g@gmail.com>
2018-06-22 15:21:00 -0500

    trying too make the fectch of the json

--- app/javascript/components/graphs.js
+++ app/javascript/components/graphs.js
@@ -1,35 +1,27 @@
-// t_data =
-new Morris.Bar({
-  // // ID of the element in which to draw the chart.
+const datos = fetch('/report_data')
+  .then(response => response.json)
+  .then((data) => {
+    return data;
+});
+
+
+console.log(datos);
+
+
+// console.log(datos);
+
+Morris.Bar({
   element: 'amount_per_user',
-  // // Chart data records -- each entry in this array corresponds to a point on
-  // // the chart.
-  // data: [
-  //   { "zone": 'Zona 1', "amount": 4000},
-  //   { "zone": 'Zona 2', "amount": 4000},
-  //   { "zone": 'Zona 3', "amount": 0}
-  //   ],
-  data: document.querySelector('#amount_per_user').dataset.tickets,
-  // // The name of the data record attribute that contains x-values.
-  // label: 'zone',
-  // value: 'amount',
-  // // A list of names of data record attributes that contain y-values.
-  // // ykeys: ['value'],
-  // // Labels for the ykeys -- will be displayed when you hover over the
-  // // chart.
-  // labels: ['PZ'],
-  // element: 'amount_per_user',
-  // data: [
-  //   { y: '2006', a: 100, b: 90 },
-  //   { y: '2007', a: 75,  b: 65 },
-  //   { y: '2008', a: 50,  b: 40 },
-  //   { y: '2009', a: 75,  b: 65 },
-  //   { y: '2010', a: 50,  b: 40 },
-  //   { y: '2011', a: 75,  b: 65 },
-  //   { y: '2012', a: 100, b: 90 }
-  // ],
-  xkey: 'zone',
-  ykeys: ['amount'],
+  data: [
+    { y: '2006', a: 100, b: 90 },
+    { y: '2007', a: 75,  b: 65 },
+    { y: '2008', a: 50,  b: 40 },
+    { y: '2009', a: 75,  b: 65 },
+    { y: '2010', a: 50,  b: 40 },
+    { y: '2011', a: 75,  b: 65 },
+    { y: '2012', a: 100, b: 90 }
+  ],
+  xkey: 'y',
+  ykeys: ['a', 'b'],
   labels: ['Series A', 'Series B']
 });
-// });
