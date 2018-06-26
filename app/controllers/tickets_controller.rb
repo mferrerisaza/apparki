02d11891cd03e221aef2f112f10ffde1dca7b11a
Miguel Ferrer <miguef7@gmail.com>
2018-06-26 16:29:20 -0500

    fix ticket order

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,9 +5,10 @@ class TicketsController < ApplicationController
   def index
     @tickets = Ticket.where(nil)
     @tickets = @tickets.search_by_plate(params[:plate]) if params[:plate].present?
-    @open_tickets = policy_scope(@tickets).where(status: "pendiente")
-    today = Time.now.strftime("%Y%m%d")
-    @close_tickets = policy_scope(@tickets).where(status: "pagado").select{ |t| t.entry.strftime("%Y%m%d") == today}
+    @open_tickets = policy_scope(@tickets).where(status: "pendiente").order(entry: :asc)
+    beginning_of_day = Time.zone.now.beginning_of_day
+    end_of_day = Time.zone.now.end_of_day
+    @close_tickets = policy_scope(@tickets).where(status: "pagado").where(exit: beginning_of_day..end_of_day).order(exit: :desc)
   end
 
   def show
