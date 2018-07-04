bf5477ee87b0962222e90c7bc22649af5d88a2bd
Miguel Ferrer <miguef7@gmail.com>
2018-07-04 10:46:18 -0500

    monetize debt cents

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -4,6 +4,8 @@ class Vehicle < ApplicationRecord
   validates :plate, presence: true
   validates :plate, uniqueness: true
 
+  monetize :debt_cents
+
   col_plate_regex = /([A-Z]{3}\d{3})|([A-Z]{2}\d{4})|([A-Z]{1}\d{4})|([A-Z]{3}\d{2}[A-Z])/
   validates :plate, format: { with: col_plate_regex }
 end
