edaa5e396d74501030bf64d42717c3103100b5fd
Miguel Ferrer <miguef7@gmail.com>
2018-07-04 10:45:26 -0500

    add update method to add debt and report ticket

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -11,4 +11,27 @@ class VehiclesController < ApplicationController
   def create
 
   end
+
+  def update
+    vehicle = Vehicle.find(params[:id])
+    ticket = Ticket.find(vehicle_params[:ticket_ids])
+    ticket.exit = Time.zone.now
+    vehicle.debt = ticket.update_charge
+    ticket.status = "reportado"
+    authorize vehicle
+    if vehicle.save && ticket.save
+      flash[:notice] = "Vehículo repotado con éxito"
+      redirect_to ticket_path(ticket)
+    else
+      flash[:alert] = "Ha ocurrido un error, porfavor inténtalo nuevamente"
+      render 'tickets/show'
+    end
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:ticket_ids)
+  end
+
 end
