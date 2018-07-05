f36b8bde34fbd8352d1c7de21ff59b3b08a561e6
Miguel Ferrer <miguef7@gmail.com>
2018-07-04 20:31:01 -0500

    add methods for checking debt and cleaning it

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -8,4 +8,14 @@ class Vehicle < ApplicationRecord
 
   col_plate_regex = /([A-Z]{3}\d{3})|([A-Z]{2}\d{4})|([A-Z]{1}\d{4})|([A-Z]{3}\d{2}[A-Z])/
   validates :plate, format: { with: col_plate_regex }
+
+  def debt?
+    !debt.zero?
+  end
+
+  def clean_debt
+    tickets =  Ticket.where(vehicle: self).where(status: "reportado")
+    tickets.each { |ticket| ticket.update(status: "pagado") }
+    self.update(debt: 0)
+  end
 end
