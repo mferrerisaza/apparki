c9bf302d0ab87098ffa0bce56d9aa116969d70d0
Miguel Ferrer <miguef7@gmail.com>
2018-07-12 15:04:33 -0500

    mix textfield and select, fix problems with js to made them work correctly and set interval to made it work on form render with errors

--- app/javascript/components/textField.js
+++
@@ -1,8 +0,0 @@
-import {MDCTextField} from '@material/textfield';
-
-const textfieldHTML = document.querySelector('.mdc-text-field');
-
-if (textfieldHTML) {
-  const textField = new MDCTextField(textfieldHTML);
-
-}
